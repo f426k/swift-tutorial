7e647d7c99088625bbed584f6d5f8eb97891a0a4
f426k <gaodaofujia@gmail.com>
2023-07-07 22:47:46 +0900

    first-commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 高嶋芙佳 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
